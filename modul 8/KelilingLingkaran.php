9e50b2a0b3aa1cf02eac38780bdc8208a01f4291
Ghiiyast <tuanghiyast@gmail.com>
2026-05-25 10:17:36 +0700

    Latihan Keliling Lingkaran dari Modul 8

---
+++ modul 8/KelilingLingkaran.php
@@ -0,0 +1,12 @@
+<?php
+
+$r = 15;
+
+define("PI", 3.14);
+
+$keliling = 2 * PI * $r;
+
+echo "Jari-jari lingkaran = " . $r . " cm<br>";
+echo "Rumus Keliling = 2 * PI * r <br>";
+echo "Maka Keliling Lingkaran adalah = <b>" . $keliling . " cm</b>";
+?>
