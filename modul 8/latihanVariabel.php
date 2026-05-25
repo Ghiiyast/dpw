0859bbe661623d59f319c99c13648dd1155d7b3a
Ghiiyast <tuanghiyast@gmail.com>
2026-05-25 10:13:55 +0700

    Latihan Variabel dari Modul 8

---
+++ modul 8/latihanVariabel.php
@@ -0,0 +1,13 @@
+<?php
+
+$daftar_hari = array("Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu");
+
+echo "<h3>Daftar Nama Hari:</h3>";
+echo "Hari 1: " . $daftar_hari[0] . "<br>";
+echo "Hari 2: " . $daftar_hari[1] . "<br>";
+echo "Hari 3: " . $daftar_hari[2] . "<br>";
+echo "Hari 4: " . $daftar_hari[3] . "<br>";
+echo "Hari 5: " . $daftar_hari[4] . "<br>";
+echo "Hari 6: " . $daftar_hari[5] . "<br>";
+echo "Hari 7: " . $daftar_hari[6] . "<br>";
+?>
